603e2024a1343cef55317e5e4f0efa56285a9565
nnsncl <nicolas.mgta@gmail.com>
2020-08-31 21:04:53 +0200

    Edit card component

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,12 +1,19 @@
 import React from 'react'
-import { Container, Image, ContentFrame, Grid } from './styles/Card'
+import {
+    Container,
+    Image,
+    ContentFrame,
+    Frame,
+    Title,
+    Subtitle
+} from './styles/Card'
 
 export default function Card({ children, ...restProps }) {
     return <Container {...restProps} >{children}</Container>
 }
 
-Card.Grid = function CardGrid({ children, ...restProps }){
-    return <Grid {...restProps} >{children}</Grid>
+Card.Frame = function CardGrid({ children, ...restProps }){
+    return <Frame {...restProps} >{children}</Frame>
 }
 
 Card.Image = function CardImage({ src, alt, ...restProps }) {
@@ -15,4 +22,12 @@ Card.Image = function CardImage({ src, alt, ...restProps }) {
 
 Card.ContentFrame = function CardContentFrame({ children, ...restProps }) {
     return <ContentFrame {...restProps} >{children}</ContentFrame>
+}
+
+Card.Title = function CardTitle({ children, ...restProps }) {
+    return <Title {...restProps} >{children}</Title>
+}
+
+Card.Subtitle = function CardSubtitle({ children, ...restProps }) {
+    return <Subtitle {...restProps} >{children}</Subtitle>
 }
--- src/components/Card/styles/Card.js
+++ src/components/Card/styles/Card.js
@@ -1,16 +1,48 @@
 import styled from 'styled-components'
 
-export const Container = styled.div`
+
+export const Frame = styled.div`
     display: flex;
-    flex-direction: column;
-    align-items: center;
+    flex-direction: row;
+    justify-content: space-between;
+
+    @media(max-width: 813px){
+        flex-direction: column;    
+    }
+`;
+
+
+
+export const Container = styled.div`
+    margin-right: 13px;
+    height: auto;
+    
+
+    &:last-of-type {
+        margin-right: 0;
+    }
+
+    @media(max-width: 813px){
+        margin-right: 0;
+    }
 `;
 
 export const Image = styled.img`
     border-radius: 23px;
+    width: 100%;
 `;
 
 export const ContentFrame = styled.div`
-    padding: 13px 0px;
-    margin: 0;
+    text-align: center;
+`;
+
+export const Title = styled.p`
+    font-size: 2.3rem;
+    font-weight: 600;
+    margin: 13px 0 9px 0;
+`;
+
+export const Subtitle = styled.p`
+    font-size: 1.3rem;
+    font-weight: 600;
 `;
