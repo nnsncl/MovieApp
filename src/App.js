cb321d4da9ec35326a7900dd9a069c022da12fa1
Nunes <nicolas.mgta+git@gmail.com>
2020-08-31 12:04:42 +0200

    Project scaffolding (auth, router)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Home, SignIn, SignUp } from './pages';
+import { AuthProvider } from './auth/Auth';
+import PrivateRoute from './helpers/PrivateRoute';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-    </div>
+    <AuthProvider>
+    {/* Everything nested in AuthProvider will have access to currentUser through the context API */}
+      <Router>
+        {/* Wrap the routes in Router which provides browsing context */}
+        <div>
+          <PrivateRoute exact path='/' component={Home} />
+          {/* Set Home as a Private route. This Route will be available only for authenticated users */}
+          <Route exact path='/signin' component={SignIn} />
+          <Route exact path='/signup' component={SignUp} />
+        </div>
+      </Router>
+    </AuthProvider>
   );
-}
-
-export default App;
+};
