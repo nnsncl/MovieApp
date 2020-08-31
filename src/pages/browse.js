d1d386ac8b3e4e5d828a8aef4c1f0dc588510b5c
Nunes <nicolas.mgta+git@gmail.com>
2020-08-31 14:16:05 +0200

    Browse page (Fetch scaffolding)

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import firebaseApp from '../firebase';
+import axios from 'axios'
 
 export default function Browse() {
+    const [movies, setMovies] = useState([])
+
+    useEffect(() => {
+
+        try {
+            const fetchData = async () => {
+                const response = await axios.get('http://localhost:3000/movies');
+                setMovies(response.data);
+            }
+            fetchData();
+            
+        } catch(error) {
+            console.log(error.message)
+        }
+   
+    }, []);
+
     return (
         <>
             <h1>Browse</h1>
             <button onClick={() => firebaseApp.auth().signOut()} >Sign out</button>
+            {movies.map(item =>
+                <p key={item.id}>{item.title}</p>
+            )}
             {/* Call signOut from auth module to log out the user on click using the firebase API */}
         </>
     );
