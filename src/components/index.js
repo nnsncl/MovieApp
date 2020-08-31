a797bdca2b453a574ac092cfa5ebe13e590243f8
nnsncl <nicolas.mgta@gmail.com>
2020-08-31 23:41:48 +0200

    Export component

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
-export { default as Navbar } from './Navbar/index';
-export { default as Heading } from './Heading/index';
-export { default as Card } from './Card/index';
 export { default as Grid } from './Grid/index'
+export { default as Navbar } from './Navbar/index'
+export { default as Heading } from './Heading/index'
+export { default as Card } from './Card/index'
+export { default as ImageSection } from './ImageSection/index'
