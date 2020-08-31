2504aeedaa9728ea3d338801dab8a7a4fe76200c
nnsncl <nicolas.mgta@gmail.com>
2020-08-31 23:40:25 +0200

    Add containers

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import { BrowseContainer } from '../containers/BrowseContainer'
+import { NavigationContainer, BrowseContainer, DistantMovieContainer } from '../containers'
 
 export default function Browse() {
     return (
         <>
+        <NavigationContainer />
+        <DistantMovieContainer />
         <BrowseContainer />
         </>
     );
