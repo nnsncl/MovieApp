cfc4fbab9957eee2952a93d9f64fd3f56b8e657c
nnsncl <nicolas.mgta@gmail.com>
2020-08-31 21:05:09 +0200

    Add New components to BrowseContainer

--- src/containers/BrowseContainer.js
+++ src/containers/BrowseContainer.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { Navbar, Card } from '../components'
+import { Navbar, Card, Grid, Heading } from '../components'
 
 import firebaseApp from '../firebase';
 import axios from 'axios'
@@ -12,6 +12,7 @@ export function BrowseContainer() {
             const fetchData = async () => {
                 const response = await axios.get('http://localhost:3000/movies');
                 setMovies(response.data);
+                console.log(response.data)
             }
             fetchData();
 
@@ -25,20 +26,70 @@ export function BrowseContainer() {
         <>
             <Navbar>
                 <Navbar.Frame>
-                    <p>Navbar</p>
+                    <Navbar.Logotype>MovieBoard</Navbar.Logotype>
                     <Navbar.Button onClick={() => firebaseApp.auth().signOut()} >
                         Sign out
                     </Navbar.Button>
                 </Navbar.Frame>
             </Navbar>
-            {movies.map(item =>
-                <Card key={item.id} >
-                    <Card.Image src={item.poster} alt={item.title} />
-                    <Card.ContentFrame>
-                        <p >{item.title}</p>
-                    </Card.ContentFrame>
-                </Card>
-            )}
+            <Grid maxFreeze={'true'} >
+                <Grid.Row>
+                    <Grid.Col size={1} >
+                        <Heading>
+                            <Heading.Flex>
+                                <Heading.Subtitle>Votre liste</Heading.Subtitle>
+                                <Heading.Button>Ajouter un film</Heading.Button>
+                            </Heading.Flex>
+                        </Heading>
+                    </Grid.Col>
+                </Grid.Row>
+                <Grid.Row>
+                    <Grid.Col size={1} >
+                        <Card.Frame>
+
+                        {movies.map(item =>
+                                <Card key={item.id} >
+                                    <Card.Image src={item.poster} alt={item.title} />
+                                    <Card.ContentFrame>
+                                        <Card.Title>{item.title}</Card.Title>
+                                    </Card.ContentFrame>
+                                </Card>
+                            )}
+
+                            <Card>
+                                <Card.Image src='https://image.tmdb.org/t/p/w342/zrNKUa5SBUwue39coJArNdDgQJM.jpg' alt='img' />
+                                <Card.ContentFrame>
+                                    <Card.Title >Title</Card.Title>
+                                </Card.ContentFrame>
+                            </Card>
+                            <Card>
+                                <Card.Image src='https://image.tmdb.org/t/p/w342/zrNKUa5SBUwue39coJArNdDgQJM.jpg' alt='img' />
+                                <Card.ContentFrame>
+                                    <Card.Title >Title</Card.Title>
+                                </Card.ContentFrame>
+                            </Card>
+                            <Card>
+                                <Card.Image src='https://image.tmdb.org/t/p/w342/zrNKUa5SBUwue39coJArNdDgQJM.jpg' alt='img' />
+                                <Card.ContentFrame>
+                                    <Card.Title >Title</Card.Title>
+                                </Card.ContentFrame>
+                            </Card>
+                            <Card>
+                                <Card.Image src='https://image.tmdb.org/t/p/w342/zrNKUa5SBUwue39coJArNdDgQJM.jpg' alt='img' />
+                                <Card.ContentFrame>
+                                    <Card.Title >Title</Card.Title>
+                                </Card.ContentFrame>
+                            </Card>
+                            <Card>
+                                <Card.Image src='https://image.tmdb.org/t/p/w342/zrNKUa5SBUwue39coJArNdDgQJM.jpg' alt='img' />
+                                <Card.ContentFrame>
+                                    <Card.Title >Title</Card.Title>
+                                </Card.ContentFrame>
+                            </Card>
+                        </Card.Frame>
+                    </Grid.Col>
+                </Grid.Row>
+            </Grid>
         </>
     )
 }
