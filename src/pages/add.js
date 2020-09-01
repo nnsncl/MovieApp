9cb666fb9b84a8c6de44466bd021faa7b92467b3
nnsncl <nicolas.mgta@gmail.com>
2020-09-01 21:23:40 +0200

    Add fuse.js scaffolding

--- src/pages/add.js
+++ src/pages/add.js
@@ -1,10 +1,51 @@
-import React from 'react';
-import { NavigationContainer } from '../containers'
+import React, { useState, useEffect } from 'react';
+// import { NavigationContainer } from '../containers'
+import { SearchInput } from '../components'
+
+// import axios from 'axios'
+import Fuse from 'fuse.js'
+
 
 export default function Add() {
+    const [query, setQuery] = useState('')
+
+    
+    const list = [
+        {
+            title: "Old Man's War",
+            author: 'John Scalzi',
+            tags: ['fiction']
+        },
+        {
+            title: 'The Lock Artist',
+            author: 'Steve',
+            tags: ['thriller']
+        }
+    ]
+
+    useEffect(() => {
+
+        const options = {
+            includeScore: true,
+            keys: ['title']
+        }
+        const fuse = new Fuse(list, options)
+        const result = fuse.search(query)
+
+        console.log(result)
+
+    }, [query])
+
+
     return (
         <>
-            <NavigationContainer />
+            {/* <NavigationContainer /> */}
+            <SearchInput>
+                <SearchInput.Field
+                    query={query}
+                    setQuery={setQuery}
+                />
+            </SearchInput>
         </>
     );
 };
