da191ce05b54ea6886d9ab56e8638b9d3e6a3968
nnsncl <nicolas.mgta@gmail.com>
2020-09-02 13:08:28 +0200

    create search container

---
+++ src/containers/SearchQueryContainer.js
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from 'react';
+// import { NavigationContainer } from '../containers'
+import { SearchInput, Card, Button } from '../components'
+import axios from 'axios'
+import Fuse from 'fuse.js'
+
+
+export default function Add() {
+    const [query, setQuery] = useState('')
+    const [distantMovies, setDistantMovies] = useState([])
+
+    useEffect(() => {
+        if (query.length > 0) {
+            const fetchDistantData = async () => {
+                const response = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&query=${query}&page=1&include_adult=false`);
+                setDistantMovies(response.data.results);
+            }
+            fetchDistantData();
+        }
+
+        const queryOptions = {
+            isCaseSensitive: false,
+            includeScore: true,
+            includeMatches: true,
+            findAllMatches: true,
+            minMatchCharLength: 0.5,
+            keys: ['title']
+        }
+
+        const queryFuse = new Fuse(distantMovies, queryOptions)
+        const queryResults = queryFuse.search(query)
+
+        console.log(queryResults)
+        // eslint-disable-next-line
+    }, [query])
+
+    return (
+        <>
+            {/* <NavigationContainer /> */}
+            <SearchInput>
+                <SearchInput.Field
+                    type='text'
+                    query={query}
+                    setQuery={setQuery}
+                />
+            </SearchInput>
+           {
+            distantMovies.map(item =>
+                    <Card key={item.id}>
+                        <Card.Image src={'http://image.tmdb.org/t/p/w342' + item.poster_path} alt={item.title} />
+                        <Card.ContentFrame>
+                            <Card.Title>{item.title}</Card.Title>
+                            <Button.Light to={'/edit/' + item.id} >Edit</Button.Light>
+                            <Button>Delete</Button>
+                        </Card.ContentFrame>
+                    </Card>)
+           }
+
+        </>
+    );
+};
--- src/pages/add.js
+++ src/pages/add.js
@@ -1,51 +1,10 @@
-import React, { useState, useEffect } from 'react';
-// import { NavigationContainer } from '../containers'
-import { SearchInput } from '../components'
+import React from 'react';
+import { SearchQueryContainer } from '../containers'
 
-import axios from 'axios'
-import Fuse from 'fuse.js'
-
-
-export default function Add() {
-    const [query, setQuery] = useState('')
-    const [distantMovies, setDistantMovies] = useState([])
-
-    useEffect(() => {
-
-        if (query.length > 0) {
-            const fetchDistantData = async () => {
-                const response = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=8028f8241d31d1afd36f7eb2de10193b&query=${query}&page=1&include_adult=false`);
-                setDistantMovies(response.data.results);
-            }
-            fetchDistantData();
-        }
-
-        const options = {
-            isCaseSensitive: false,
-            includeScore: true,
-            includeMatches: true,
-            findAllMatches: true,
-            minMatchCharLength: 0.5,
-            keys: ['title']
-        }
-
-        const fuse = new Fuse(distantMovies, options)
-        const result = fuse.search(query)
-
-        console.log(result)
-        // eslint-disable-next-line
-    }, [query])
-
-
+export default function Browse() {
     return (
         <>
-            {/* <NavigationContainer /> */}
-            <SearchInput>
-                <SearchInput.Field
-                    query={query}
-                    setQuery={setQuery}
-                />
-            </SearchInput>
+            <SearchQueryContainer />
         </>
     );
 };
