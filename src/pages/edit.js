acce8a886aeac6183645ace22f0a64a3097adb1a
nnsncl <nicolas.mgta@gmail.com>
2020-09-02 13:08:46 +0200

    create edit page scaffolding

--- src/pages/edit.js
+++ src/pages/edit.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useParams } from "react-router-dom";
+
+import axios from 'axios'
 
 export default function Edit() {
+    const params = useParams('id')
+    const [editMovie, setEditMovie] = useState([])
+    // eslint-disable-next-line
+    console.log(params.id)
+    console.log(editMovie)
+    useEffect(() => {
+        try {
+            const fetchDistantData = async () => {
+                const response = await axios.get(`https://api.themoviedb.org/3/movie/${params.id}?api_key=${process.env.REACT_APP_TMDB_KEY}`);
+                setEditMovie(response.data);
+            }
+            fetchDistantData();
+
+        } catch (error) {
+            alert(error.message)
+        }
+        // eslint-disable-next-line
+    }, []);
+
+    console.log(editMovie)
+
     return (
         <>
-            <h1>Edit</h1>
+            <h1>{editMovie.title}</h1>
         </>
     );
 };
