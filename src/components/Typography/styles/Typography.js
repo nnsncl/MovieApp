e4e31d02d27517b1e055269695f2ad88fb8cfab9
nnsncl <nicolas.mgta@gmail.com>
2020-09-04 00:28:13 +0200

    Create typography scaffolding

---
+++ src/components/Typography/styles/Typography.js
@@ -0,0 +1,31 @@
+import styled from 'styled-components'
+
+export const TitleLarge = styled.h1`
+    font-size: 9.5rem;
+    margin-bottom: 9px;
+
+    @media(max-width: 1056px){
+        font-size: 5.6rem;
+        margin-bottom: 23px;
+    }
+
+    @media(max-width: 672px){
+        font-size: 3.6rem;
+        margin-bottom: 23px;
+    }
+`;
+export const TitleMd = styled.h2``;
+export const TitleSm = styled.h3``;
+export const TitleXs = styled.h4``;
+export const TitleXxs = styled.h5``;
+export const TitleH6 = styled.h6``;
+export const BodyLarge = styled.p`
+    font-size: 2.3rem;
+    margin-bottom: 36px;
+
+    @media(max-width: 1056px){
+        font-size: 1.6rem;
+    }
+`;
+export const Body = styled.p``;
+export const BodySmall = styled.p``;
