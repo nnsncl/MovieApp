1ac134c819751cb5c454b7f06f2474ffbbd13921
nnsncl <nicolas.mgta@gmail.com>
2020-09-03 23:58:39 +0200

    Containers adjustments

--- src/containers/MovieDetailContainer.js
+++ src/containers/MovieDetailContainer.js
@@ -28,7 +28,14 @@ export default function MovieDetailContainer() {
                                             {filteredMovie.categories.map(item => <Chips key={uuidv4()} >{item}</Chips>)}
                                         </Grid.Row>
                                         <Section>
-                                            <Button.Large>Remove from my list</Button.Large>
+                                            <Button.Large>
+                                                <svg aria-hidden="true" height="13px" width='13px' transform="rotate(360)" viewBox="0 0 512 512">
+                                                    <defs />
+                                                    <rect width="448" height="80" x="32" y="48" fill="#f3f3f4" rx="32" ry="32" />
+                                                    <path fill="white" d="M74.45 160a8 8 0 00-8 8.83l26.31 252.56a1.5 1.5 0 000 .22A48 48 0 00140.45 464h231.09a48 48 0 0047.67-42.39v-.21l26.27-252.57a8 8 0 00-8-8.83zm248.86 180.69a16 16 0 11-22.63 22.62L256 318.63l-44.69 44.68a16 16 0 01-22.63-22.62L233.37 296l-44.69-44.69a16 16 0 0122.63-22.62L256 273.37l44.68-44.68a16 16 0 0122.63 22.62L278.62 296z" />
+                                                </svg>&nbsp;
+                                            Remove from my list
+                                            </Button.Large>
                                         </Section>
                                     </Grid.Col>
                                     <Grid.Col size={1} breakPoint='md' >
