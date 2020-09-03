1fe7e28f84ae66ce38b36158995c888d424b6924
nnsncl <nicolas.mgta@gmail.com>
2020-09-03 13:21:10 +0200

    Create chips component

---
+++ src/components/Chips/styles/Chips.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+export const Chips = styled.p`
+    font-size: 1.3rem;
+    background: rgba(255, 255, 255, 0.3);
+    color: white;
+    font-weight: bold;
+    padding: 13px 19px;
+    border-radius: 56px;
+    margin-right: 13px;
+    margin-bottom: 13px;
+
+    &:last-of-type {
+        margin-right: 0;
+    }
+`;
