d79a65e089b9da8156ae1980ac60f82990117bdd
nnsncl <nicolas.mgta@gmail.com>
2020-09-03 13:22:24 +0200

    Update card component

--- src/components/Card/styles/Card.js
+++ src/components/Card/styles/Card.js
@@ -1,29 +1,36 @@
 import styled from 'styled-components'
 
+export const Container = styled.div`
+    margin-right: 13px;
+    margin-bottom: 23px;
+    height: auto;
+    
+    &:last-of-type {
+        margin-right: 0;
+    }
+
+    @media(max-width: 672px){
+        margin-right: 0;
+    }
+`;
 
 export const Frame = styled.div`
     display: flex;
     flex-direction: row;
     justify-content: space-between;
 
-    @media(max-width: 813px){
+    @media(max-width: 672px){
         flex-direction: column;    
     }
 `;
 
-
-export const Container = styled.div`
-    margin-right: 13px;
+export const FixedWidth = styled.div`
+    width: 25%;
+    margin-right: 23px;
     margin-bottom: 23px;
-    height: auto;
-    
 
-    &:last-of-type {
-        margin-right: 0;
-    }
-
-    @media(max-width: 813px){
-        margin-right: 0;
+    @media(max-width: 641px) {
+        width: 42%;
     }
 `;
 
@@ -32,6 +39,7 @@ export const Image = styled.img`
 `;
 
 export const ContentFrame = styled.div`
+    margin-top: 23px;
 `;
 
 export const Title = styled.p`
